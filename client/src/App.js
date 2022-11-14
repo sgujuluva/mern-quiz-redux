d1d30d6e3eb21d5098d913e9d1689ed19f55098a
sgujuluva <sanghee04@gmail.com>
2022-11-14 20:02:10 +0100

    components created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import {Routes, Route} from "react-router-dom"
+//routes
+import Main from "./components/Main"
+import Quiz from "./components/Quiz"
+import Result from "./components/Result"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <h1>Quiz app</h1>
+     <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result/>} />
+      </Routes>
     </div>
   );
 }
